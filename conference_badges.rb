94121ac82801d5604a74a2fa7eae6fdfeec36333
Anna Dybas <a.m.dybas@gmail.com>
2019-05-18 20:19:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,11 @@ def batch_badge_creator(attendees)
   end
 end
 
-def assign_rooms()
+def assign_rooms(attendees)
+  attendees.each_with_index do |name, index|
+      "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+
 end
 
 def printer()
